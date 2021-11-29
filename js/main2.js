3a45c34a1e1afb7f04ebc78829517df5a4d2417f
Zamir Kanji <zamirkanji@comcast.net>
2021-11-29 11:44:12 -0800

    touch action, new js code, clear entry

--- js/main2.js
+++ js/main2.js
@@ -12,16 +12,18 @@ const subtractBtn = document.querySelector('.subtract-btn');
 const additionBtn = document.querySelector('.addition-btn');
 const equalsBtn = document.querySelector('.equals-btn');
 
-//********************************************** LOGIC **********************************************
-let displayInput = 0;
-let displayCurrent = 0;
+const decimalBtn = document.querySelector('.btn-decimal');
 
-let numArr = [];
+//********************************************** VARIABLES **********************************************
 
 const divideOperator = ' / ';
 const multiplyOperator = ' * ';
 const subtractOperator = ' - ';
 const additionOperator = ' + ';
+
+let displayInput = 0;
+let displayCurrent = 0;
+let numArr = [];
 
 let stringNums = '';
 
@@ -29,32 +31,21 @@ let addBtnClicked = false;
 let subtractBtnClicked = false;
 let multiplyBtnClicked = false;
 let divideBtnClicked = false;
-
-//windows on load show default 0
-// window.onload = (e) => {
-//     displayText.textContent = displayInput;
-//     displayText.textContent = displayCurrent;
-// }
-
-//*********************************CHECK IF MOBILE************************************
-
-let mobile = navigator.userAgent;
-const isIOS = () => {
-    return (
-        /iPad|iPhone|iPod/.test(mobile)
-    );
-}
-let isMobile = isIOS();
-console.log(isMobile);
+let decimalBtnClicked = false;
 
 //************************************ OPERAND BTN EVENTS/DISPLAY NUMBERS *************************************** */
-allNumberBtns.forEach(btn =>  {
-    btn.addEventListener('click', (e) => {
-        let val = e.target.value;
-        displayText.textContent = val;
-        displayText.textContent = stringNums +=  val;       
+
+const displayNum = (e) => {
+    clearEntry();
+    allNumberBtns.forEach(btn =>  {
+        btn.addEventListener('click', (e) => {
+            let val = e.target.value;
+            displayText.textContent = val;
+            displayText.textContent = stringNums +=  val;       
+        })
     })
-})
+    return stringNums;
+}
 //************************************* STORE NUMBER, OPERATOR ****************************** */
     
 function storeOperator_Num (number, operator) {
@@ -64,31 +55,29 @@ function storeOperator_Num (number, operator) {
 }
 
 //************************************* OPERATION FUNCTIONS ************************************ */
+
 const divideNums = (a, b) => {
     console.log(a, b);
-    let answer = +a / +b;
+    // let answer = Math.round((a / b));
+    let answer = a / b;
     displayText.textContent = answer;
     numArr = [];
-    numArr.push(answer);
-    console.log(numArr);
     stringNums = answer;
     return answer; 
 }
 const multiplyNums = (a, b) => {
     console.log(a, b);
-    let answer = +a * +b;
+    let answer = a * b;
     displayText.textContent = answer;
     numArr = [];
-    // numArr.push(answer);
     stringNums = answer;
     return answer; 
 }
 const subtractNums = (a, b) => {
     console.log(a, b);
-    let answer = +a - +b;
+    let answer = a - b;
     displayText.textContent = answer;
     numArr = [];
-    // numArr.push(answer);
     stringNums = answer;
     return answer; 
 }
@@ -97,7 +86,6 @@ const addNums = (a, b) => {
     let answer = +a + +b;
     displayText.textContent = answer;
     numArr = [];
-    // numArr.push(answer);
     stringNums = answer;
     return answer; 
 }
@@ -105,16 +93,17 @@ const addNums = (a, b) => {
 //**********************************OPERATION ************************************** */
 
 function operation (a, b) {
-    // numArr.push(stringNums);
     if (divideBtnClicked == true) {
         divideBtn.classList.remove('highlight-operator');
         divideNums(a, b);
-        // console.log(divide_Answer);
     } else if (multiplyBtnClicked == true) {
-        const multiply_Answer = multiplyNums(a, b);
+        multiplyBtn.classList.remove('highlight-operator');
+        multiplyNums(a, b);
     } else if (subtractBtnClicked == true) {
+        subtractBtn.classList.remove('highlight-operator');
         subtractNums(a, b);
     } else if (addBtnClicked == true) {
+        additionBtn.classList.remove('highlight-operator');
         addNums(a, b);
     } else {
         return;
@@ -125,15 +114,20 @@ function operation (a, b) {
 // ************************* CLEAR ALL, CLEAR ENTRY **************************************
 
 const clearAll = () => {
-    // tempArr = [];
-    // displayText.textContent = displayCurrent;
-    // displayText.classList.toggle('vis-hidden');
     let clearAllConfirm = window.confirm('Are you sure you want to Clear Everything?');
     if (clearAllConfirm) {location.reload()};
 }
 
+const clearEntry = () => {
+    clearEntryBtn.addEventListener('click', () => {
+        stringNums = stringNums.substring(0, stringNums.length - 1);
+        displayText.textContent = stringNums;
+    })
+    
+}
 
 //**************************** OPERATOR EVENT LISTENERS ************************************ */
+
 divideBtn.addEventListener('click', (e) => {
     divideBtnClicked = true;
     divideBtn.classList.add('highlight-operator');
@@ -142,43 +136,38 @@ divideBtn.addEventListener('click', (e) => {
 });
 multiplyBtn.addEventListener('click', (e) => {
     multiplyBtnClicked = true;
+    multiplyBtn.classList.add('highlight-operator');
     numArr.push(stringNums);
     storeOperator_Num(stringNums, multiplyOperator);
 });
 subtractBtn.addEventListener('click', (e) => {
     subtractBtnClicked = true;
+    subtractBtn.classList.add('highlight-operator');
     numArr.push(stringNums);
     storeOperator_Num(stringNums, subtractOperator);
 });
 additionBtn.addEventListener('click', (e) => {
     addBtnClicked = true;
+    additionBtn.classList.add('highlight-operator');
     numArr.push(stringNums);
     storeOperator_Num(stringNums, additionOperator);
 });
 
-
-//EQUALS BUTTONS
-
-equalsBtn.addEventListener('click', () => {
-    // storeOperator_Num(numArr, stringNums);
+//DECIMAL
+decimalBtn.addEventListener('click', () => {
+    decimalBtnClicked = true;
     
-    // if (numArr.length == 2) {
-    //     numArr = [];
-    //     // numArr.push(answer);
-    //     stringNums = answer;
-    //     console.log(numArr, stringNums);
-    // } else {
-        numArr.push(stringNums);
-        operation(numArr[0], numArr[1]);
-    // }
 });
 
+//***************************** EQUALS BUTTON EVENT LISTENER ************************************ */
+
+equalsBtn.addEventListener('click', () => {
+    numArr.push(+stringNums);
+    console.log(`Array value: ${numArr}`);
+    operation(numArr[0], numArr[1]);
+});
 
 //CLEAR ALL BUTTON 
-if (isMobile) {
-    clearAllBtn.addEventListener('touchstart', clearAll);
-} else {
-    clearAllBtn.addEventListener('click', clearAll);
-}
+clearAllBtn.addEventListener('click', clearAll);
 
-//CLEAR ENTRY (get last version num pushed to calculator (from storeNum()) and clear it 
+displayNum();
